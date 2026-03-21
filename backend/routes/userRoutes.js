87e4e25a99858fe2794099ad8399ae0448f33adc
Krishna <srikrishna19701@gmail.com>
2026-03-21 17:47:37 +0530

    Routes Updated

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../schema/userSchema');
+
+// Create a new user
+router.post('/signup', async (req,res)=>{
+    try{
+        const {username, password, email} = req.body;
+        const user = new User({ username, password, email });
+        await user.save();
+        res.status(201).json({ message: 'User created successfully' });
+    }catch(err){
+        console.error('Error creating user:', err);
+        res.status(500).json({ message: 'Server error' });
+    }
+})
+
+// login
+router.post('login', async (req,res)=>{
+    try{
+        const {username,password} = req.body;
+        const user = await USer.findOne({username,password});
+        if(user){
+            res.message('Login successful');
+        }else{
+            res.status(401).json({message:'Invalid credentials'});
+        }
+    }catch(err){
+        console.error('Error during login:', err);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
